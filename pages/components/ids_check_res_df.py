2b45142431f9db1385dbfb61324078f915675ec1
tmlnv <tmlnv@protonmail.com>
2024-03-10 17:04:29 +0300

    Updated styling and inner function to handle passed percentage in 3.xxx fromat float and also in oragne color is < 50

--- pages/components/ids_check_res_df.py
+++ pages/components/ids_check_res_df.py
@@ -33,17 +33,18 @@ def create_specifications_dataframe(data: dict):
     # Create DataFrame
     df = pd.DataFrame(rows)
 
-    def _calc_passed_percentage(row_) -> int | None:
-        # Calculate percentages and add as a new column
-        if row_['Total Checks'] <= 0:
+    def _calc_passed_percentage(row_) -> str | None:
+        """Calculate the total passed checks percentage of a specification"""
+        # Check for zero total checks to avoid division by zero
+        if row_['Total Checks'] == 0:
             return None
         else:
-            try:
-                return (row_['Passed Checks'] / row_['Total Checks'] * 100).round().astype(int)
+            # Calculate the percentage
+            percentage = (row_['Passed Checks'] / row_['Total Checks']) * 100
+            # Return the percentage with three decimal places
+            return f"{percentage:.3f}"
 
-            except Exception:
-                return row_['Passed Checks'] / row_['Total Checks'] * 100
-
+    # Apply the function to calculate 'Passed Percentage'
     df['Passed Percentage'] = df.apply(_calc_passed_percentage, axis=1)
 
     # Add summary row
@@ -59,16 +60,19 @@ def create_specifications_dataframe(data: dict):
     df = df.append(summary_row, ignore_index=True)
 
     # Define a function to apply colors
-    def color_pass_fail(val):
+    def color_pass_fail(val: str | None) -> str:
         if pd.isnull(val):  # check for NaN or None
             return 'background-color: gray'
 
-        if isinstance(val, (float, int)):  # apply only to numeric columns
-            if val < 50:  # assuming a fail if less than 50% pass
+        else:  # apply only to numeric columns
+            val = float(val)
+            if val < 30:  # assuming a fail if less than 50% pass
                 return 'background-color: red'
-            elif val < 80:  # assuming a fail if less than 50% pass
+            if val < 50:  # assuming a fail if less than 50% pass
+                return 'background-color: orange'
+            elif val < 70:  # assuming a fail if less than 50% pass
                 return 'background-color: yellow'
-            elif val >= 80:
+            elif val >= 70:
                 return 'background-color: green'
 
         return ''  # return empty string for other types
