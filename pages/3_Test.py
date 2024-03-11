0b62d595734c7c11142ed7a9579e4e612e46c862
tmlnv <tmlnv@protonmail.com>
2024-03-11 19:20:37 +0300

    Updated run ids test button, added spinner

--- pages/3_Test.py
+++ pages/3_Test.py
@@ -1,9 +1,6 @@
 import json
-from time import sleep
 
 import streamlit as st
-import numpy as np
-import pandas as pd
 from ifctester import ids, reporter
 
 from pages.components.constants import MSG_UPLOAD_FILE_REQ
@@ -14,70 +11,6 @@ from pages.components.load_data import load_data
 session = st.session_state
 
 
-def draw_chart():
-    chart_data = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['a', 'b', 'c'])
-
-    st.line_chart(chart_data)
-
-
-def plot_map():
-    map_data = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [59.93, 30.31],
-        columns=['lat', 'lon'])
-
-    st.map(map_data)
-
-
-def write_widget():
-    x = st.slider('x')  # 👈 this is a widget
-    return x
-
-
-def input_name():
-    st.text_input("Your name", key='name')
-    st.write(st.session_state.name)
-
-
-def checkbox():
-    if st.checkbox('Show dataframe', True):
-        chart_data = pd.DataFrame(
-            np.random.randn(20, 3),
-            columns=['a', 'b', 'c'])
-
-        st.write(chart_data)
-
-
-def selectbox():
-    df = pd.DataFrame({
-        'first column': [1, 2, 3, 4],
-        'second column': [10, 20, 30, 40]
-    })
-
-    option = st.selectbox(
-        'Which number do you like best?',
-        df['first column'])
-
-    st.write('You selected: ', option)
-
-
-def show_progress():
-    st.write('Starting...')
-
-    # Add a placeholder
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-
-    for i in range(100):
-        # Update the progress bar with each iteration.
-        latest_iteration.text(f'Iteration {i + 1}')
-        bar.progress(i + 1)
-        sleep(0.01)
-
-    st.write('...and now we\'re done!')
-
-
 def initialize_session_state():
     session["DataFrame"] = None
     session["Classes"] = []
@@ -86,29 +19,6 @@ def initialize_session_state():
         session["IdsFile"] = None
     session["IdsReport"] = None
     session["IdsReportDF"] = None
-
-
-def run_tests():
-    show_progress()
-    st.write(dir(session.DataFrame))
-    st.write(session.DataFrame.get('Type'))
-    obj_types = session.DataFrame.get('Type')
-    st.write(type(obj_types))
-
-    counter = 0
-    for elem in session.DataFrame.get('Type'):
-        try:
-            test_type_of_element(elem)
-        except AssertionError as e:
-            counter += 1
-    st.write(f'Total elements failed checking {counter}')
-
-
-def test_type_of_element(element):
-    allowed_types = ['Базовая стена:Внешняя несущая', 'Прямоугольный импост:50 x 150мм', 'Перекрытие:Типовой 200мм',
-                     'Базовая стена:Перегородка из кирпича, 120']
-
-    assert element in allowed_types
 
 
 def upload_ids_file():
@@ -124,6 +34,7 @@ def print_ids_as_dict():
 
 
 def run_ids_test():
+
     ids_info = session["IdsFile"].info
 
     try:
@@ -133,14 +44,20 @@ def run_ids_test():
 
     st.write(f'Название ids спецификации: **{ids_info["title"]}**, автор: {ids_author}')
 
-    show_progress()
+    is_finished = False
 
-    session["IdsFile"].validate(session["ifc_file"])
+    while not is_finished:
 
-    report = reporter.Json(session["IdsFile"]).report()
-    session["IdsReport"] = report
+        with st.spinner('Выполняется проверка качества'):
 
-    session["IdsReportDF"] = create_specifications_dataframe(report)
+            session["IdsFile"].validate(session["ifc_file"])
+
+            report = reporter.Json(session["IdsFile"]).report()
+            session["IdsReport"] = report
+
+            session["IdsReportDF"] = create_specifications_dataframe(report)
+
+        is_finished = True
 
 
 def execute():
@@ -167,7 +84,7 @@ def execute():
 
             if session["IdsFile"]:
                 if st.button(
-                        'Run IDS tests',
+                        'Провести проверку качества',
                         key="run_ids_test",
                         help='Провести тест в соответствии с IDS спецификацией'
                 ):
@@ -175,10 +92,6 @@ def execute():
                 if session.get("IdsReportDF"):
                     st.header("Результаты проверки")
                     st.write(session["IdsReportDF"])
-
-
-            if st.button('Run tests', key="run_tests", help='Провести тест'):
-                run_tests()
 
         with tab2:
             st.header("IDS Specification")
